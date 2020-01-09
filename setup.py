05c2be6f1de217811170f631add277f80651b7a4
piEsposito <piero.skywalker@gmail.com>
2020-01-09 20:20:00 -0300

    commit

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(
     description = "Slingshot is a Python library bringed on by A Mosca team for creating reproducible procedural documents for Brazilian law-suits.",
     author = "A Mosca",
     author_email = "staff.amosca@gmail.com",
-    download_url = "https://github.com/amosca-team/slingshot/archive/0.5.tar.gz",
+    download_url = "https://github.com/amosca-team/slingshot/archive/0.5.2.tar.gz",
     install_requires = ["pdfkit",
                         "pandoc",
                         "markdown"
-                        ]
+                        ],
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers, Law students",
